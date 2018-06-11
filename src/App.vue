198d07158714a29e76f65f611ce558b915f22fcf
mbj36 <mohitbajo36@gmail.com>
2018-06-11 11:44:12 +0530

    Added more props feature

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <VueOtpInput />
+    <VueOtpInput seperator="-" :numInputs="4" />
   </div>
 </template>
 
--- src/components/VueOtpInput.vue
+++ src/components/VueOtpInput.vue
@@ -1,22 +1,53 @@
 <template>
-
+  <div class="div__style">
+    <span v-for="i in numInputs" :key="i">
+      <div>
+        <input class="__input-style" type="tel" maxLength="1" /> {{seperator}}
+      </div>
+    </span>
+  </div>
 </template>
 
 <script>
   export default {
     props: {
       numInputs: {
-        type: number,
+        type: Number,
+        default: 4,
         required: true
       },
       seperator: {
-        type: Object,
-        required: true
+        type: String,
+        required: false,
+        default: '-'
       },
       onChange: {
         type: Function,
+        required: false
+      },
+      disabled: {
+        type: Boolean,
+        required: false
+      },
+      shouldAutoFocus: {
+        type: Boolean,
         required: false
       }
     }
   };
 </script>
+
+<style>
+  .__input-style {
+    width: 1em;
+    text-align: center;
+    font-size: 16px;
+    padding: 1.5em;
+    margin: 1em;
+  }
+  .div__style {
+    display: flex;
+    margin: 0 auto;
+    width: 30%;
+  }
+</style>
